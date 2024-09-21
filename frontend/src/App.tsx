9c19ed274fa70bbe1eb5abe976c2a21f72905116
Sandip_Dutta_8 <ayandutta2005@gmail.com>
2024-09-21 18:37:27 +0530

    frontend initialized

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button';
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <Button>Click Me</Button>
+    </>
+  )
+}
+
+export default App
