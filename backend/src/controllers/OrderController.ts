f5d7ccb801347d9ef702cd7c2f21fdf8a22e6fde
Sandip_Dutta_8 <ayandutta2005@gmail.com>
2024-10-09 15:38:11 +0530

    checkout api added

---
+++ backend/src/controllers/OrderController.ts
@@ -0,0 +1,135 @@
+import { Request, Response } from "express";
+import Stripe from "stripe";
+import Restaurant, { MenuItemType } from "../models/restaurant";
+import Order from "../models/order";
+
+const STRIPE = new Stripe(process.env.STRIPE_API_KEY as string);
+const FRONTEND_URL = process.env.FRONTEND_URL as string;
+
+
+type CheckoutSessionRequest = {
+    cartItems: {
+        menuItemId: string;
+        name: string;
+        quantity: string;
+    }[];
+    deliveryDetails: {
+        email: string;
+        name: string;
+        addressLine1: string;
+        city: string;
+    };
+    restaurantId: string;
+};
+
+const createCheckoutSession = async (req: Request, res: Response) => {
+    try {
+        const checkoutSessionRequest: CheckoutSessionRequest = req.body;
+
+        const restaurant = await Restaurant.findById(
+            checkoutSessionRequest.restaurantId
+        );
+
+        if (!restaurant) {
+            throw new Error("Restaurant not found");
+        }
+
+        // const newOrder = new Order({
+        //     restaurant: restaurant,
+        //     user: req.userId,
+        //     status: "placed",
+        //     deliveryDetails: checkoutSessionRequest.deliveryDetails,
+        //     cartItems: checkoutSessionRequest.cartItems,
+        //     createdAt: new Date(),
+        // });
+
+        const lineItems = createLineItems(
+            checkoutSessionRequest,
+            restaurant.menuItems
+        );
+
+        const session = await createSession(
+            lineItems,
+            // newOrder._id.toString(),
+            "TEST_ORDER_ID",
+            restaurant.deliveryPrice,
+            restaurant._id.toString()
+        );
+
+        if (!session.url) {
+            return res.status(500).json({ message: "Error creating stripe session" });
+        }
+
+        //await newOrder.save();
+        res.json({ url: session.url });
+    } catch (error: any) {
+        console.log(error);
+        res.status(500).json({ message: error.raw.message });
+    }
+};
+
+const createLineItems = (
+    checkoutSessionRequest: CheckoutSessionRequest,
+    menuItems: MenuItemType[]
+) => {
+    const lineItems = checkoutSessionRequest.cartItems.map((cartItem) => {
+        const menuItem = menuItems.find(
+            (item) => item._id.toString() === cartItem.menuItemId.toString()
+        );
+
+        if (!menuItem) {
+            throw new Error(`Menu item not found: ${cartItem.menuItemId}`);
+        }
+
+        const line_item: Stripe.Checkout.SessionCreateParams.LineItem = {
+            price_data: {
+                currency: "inr",
+                unit_amount: menuItem.price,
+                product_data: {
+                    name: menuItem.name,
+                },
+            },
+            quantity: parseInt(cartItem.quantity),
+        };
+
+        return line_item;
+    });
+
+    return lineItems;
+};
+
+const createSession = async (
+    lineItems: Stripe.Checkout.SessionCreateParams.LineItem[],
+    orderId: string,
+    deliveryPrice: number,
+    restaurantId: string
+) => {
+    const sessionData = await STRIPE.checkout.sessions.create({
+        line_items: lineItems,
+        shipping_options: [
+            {
+                shipping_rate_data: {
+                    display_name: "Delivery",
+                    type: "fixed_amount",
+                    fixed_amount: {
+                        amount: deliveryPrice,
+                        currency: "inr",
+                    },
+                },
+            },
+        ],
+        mode: "payment",
+        metadata: {
+            orderId,
+            restaurantId,
+        },
+        success_url: `${FRONTEND_URL}/order-status?success=true`,
+        cancel_url: `${FRONTEND_URL}/detail/${restaurantId}?cancelled=true`,
+    });
+
+    return sessionData;
+};
+
+export default {
+    createCheckoutSession
+}
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,7 @@ import myUserRoute from "./routes/MyUserRoute";
 import myRestaurantRoute from "./routes/MyRestaurantRoute";
 import restaurantRoute from "./routes/RestaurantRoute";
 import { v2 as cloudinary } from 'cloudinary';
+import orderRoute from "./routes/OrderRoute";
 
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => console.log('database connected!'));
@@ -28,6 +29,7 @@ app.get("/health", async (req: Request, res: Response) => {
 app.use("/api/my/user", myUserRoute);
 app.use("/api/my/restaurant", myRestaurantRoute);
 app.use("/api/restaurant", restaurantRoute);
+app.use("api/order", orderRoute);
 
 app.listen(7000, () => {
     console.log('backend started on port 7000');
